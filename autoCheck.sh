8e5ff6edc784c5fc3c57ea9195801274156c4c94
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2018-04-25 16:58:16 +0700

    Working v1.0
    Added remaining functionality. Should work fine

--- autoCheck.sh
+++ autoCheck.sh
@@ -1,30 +1,54 @@
 #!/bin/bash
 
-labNumber=$1			#first argument
-answer=$2			#second argument
-labDir=${labNumber:0:4}		#substring labNumber to "Lab1"
+#-------------------|autoCheck.sh|---------------------------
+# A shell script done for self-study of UNIX shell scripting.
+#------------------------------------------------------------
 
-#Main program loop
+usageText=$'Usage: ./autoCheck LabXY correctOutput where,\nX is lab number and Y is question number.'  #usage prompt
+labNumber=$1  #first argument
+answer=$2  #second argument
+labDir=${labNumber:0:4}  #substring labNumber to "LabX"
+
+#display usage text if not given proper arguments or given "-help"
+if [ "$1" = "-help" ]; then
+	echo "$usageText"
+	exit 1
+elif [ "$#" -lt 2 ]; then
+	echo "Error: Invaild arguments. use -help for more information."
+	exit 1
+fi
+
+#overwrite the results if they already exist
+if [ -f ./result$labNumber.txt ]; then
+	rm result$labNumber.txt
+fi
+
+#Main program loop (for each file in the lab)
 for file in Labs/$labDir/56000*/$labNumber.c
 do
-   if [[ -f "$file"  ]]; then
-        #process the file with name in "$file"
-        if (gcc $file &>/dev/null); then   #&>/dev/null mute the error output from gcc        
-            echo -n "$compiled" >> result.txt 
-            echo -n ";" >> result.txt #compilation is sucessful
-            result=$(./a.out) #get result from running the program
+   if [[ -f "$file"  ]]; then  #if we find a source file
+        if (gcc $file &>/dev/null); then  #if compilation is successful (&>/dev/null mute the error output from gcc)      
+            echo -n "$compiled" >> result$labNumber.txt 
+            echo -n "${file:10:6};" >> result$labNumber.txt  #write studentID to results
+            result=$(./a.out)  #get result from running the program
             if [[ "$result" == $answer ]];then
                 echo "$file : correct"
-                echo "correct" >> result.txt #append correct to result file
+                echo "3" >> result$labNumber.txt  #append full score (three)
             else
                 echo "$file : wrong"
-                echo "wrong" >> result.txt #append incorrect to result file
-            fi   
-        rm a.out
+                echo "2" >> result$labNumber.txt  #append score of two
+            fi
+
+        rm a.out  #cleanup the compiled program output
         
         else
-        echo "$file : Compile error" #compilation fails
+            echo "$file : Compile error"  #compilation fails
+	    echo -n "${file:10:6};" >> result$labNumber.txt
+	    echo "1" >> result$labNumber.txt  #append score of one
 
         fi
    fi
 done
+
+#Job is done. Exit with success.
+exit 0
